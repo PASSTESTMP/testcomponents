61d1057fbfd1b1b7cff8423850ae62382a156381
Michal Polomski <michal.polomski@o2.pl>
2025-09-12 23:41:33 +0200

    spring ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/services/keyboard_key.g.dart';
 import 'package:testcomponents/spell.dart';
 
 void main() {
@@ -10,28 +13,55 @@ void main() {
   );
 }
 
-class MyEmptyGame extends FlameGame {
+class MyEmptyGame extends FlameGame with HasKeyboardHandlerComponents {
   @override
   Future<void> onLoad() async {
     Spell spell = Spell();
+    spell.size.x = size.x/2;
+    spell.size.y = size.y;
     spell.position = size / 4;
     world.add(spell);
+
+    debugMode = false;
+
     return super.onLoad();
   }
 
-  Vector2 speed = Vector2(0, 100);
+  Vector2 speed = Vector2(0, 0);
+
+  @override
+  KeyEventResult onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    if(event is KeyDownEvent){
+      if(event.logicalKey == LogicalKeyboardKey.arrowUp){
+        speed = Vector2(0, -100);
+      }
+      if(event.logicalKey == LogicalKeyboardKey.arrowDown){
+        speed = Vector2(0, 100);
+      }
+    }
+
+    if(event is KeyUpEvent){
+      speed = Vector2(0, 0);
+    }
+    return super.onKeyEvent(event, keysPressed);
+  }
 
   @override
   void update(double dt) {
     Spell targetSpell = world.children.query<Spell>().first;
     
-    if(targetSpell.position.y > 100){
-      speed = Vector2(0, -100);
-    }
-    if(targetSpell.position.y < -100){
-      speed = Vector2(0, 100);
-    }
-    targetSpell.position += speed * dt;
+    // if(targetSpell.position.y > 100){
+    //   speed = Vector2(0, -100);
+    // }
+    // if(targetSpell.position.y < -100){
+    //   speed = Vector2(0, 100);
+    // }
+    targetSpell.particles.first.position += speed * dt;
+
+
+    targetSpell.position.x = 0;
+    targetSpell.position.y = -size.y/2;
+
     super.update(dt);
   }
 }
--- lib/spell.dart
+++ lib/spell.dart
@@ -2,20 +2,28 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/extensions.dart';
 import 'package:flutter/material.dart';
 
-class Spell extends PositionComponent {
+class Spell extends PositionComponent with DragCallbacks {
   Spell() : super(size: Vector2.all(20.0));
 
   List<Particle> particles = [];
+  Vector2 speed = Vector2.zero();
 
-  int particleNumber = 20;
+  int particleNumber = 80;
 
   @override
   FutureOr<void> onLoad() {
     particles = List.generate(particleNumber, (index) => addParticle(index));
-    addAll(particles);
+    for (var particle in particles) {
+      // particle.stiffness = 50 + Random().nextDouble() * 150;
+      // particle.damping = 1 + Random().nextDouble() * 5;
+      add(particle);
+      particle.attach(particles.first);
+    }
+    removeAll(particles);
     return super.onLoad();
   }
 
@@ -28,9 +36,27 @@ class Spell extends PositionComponent {
     return particle;
   }
 
+  @override
+  void onDragStart(DragStartEvent event) {
+    addAll(particles);
+    particles.first.position = event.localPosition;
+    super.onDragStart(event);
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    particles.first.position = event.localEndPosition;
+    super.onDragUpdate(event);
+  }
+
+  @override
+  void onDragEnd(DragEndEvent event) {
+    removeAll(particles);
+    super.onDragEnd(event);
+  }
 }
 
-class Particle extends PositionComponent {
+class Particle extends PositionComponent with SpringJoint {
 
   Particle(this.index, this.stableDistance) : super(size: Vector2.all(5.0));
 
@@ -72,4 +98,61 @@ class Sparkle extends RectangleComponent {
     super.update(dt);
   }
 
+}
+
+mixin SpringJoint on PositionComponent {
+  /// Obiekt do którego jesteśmy połączeni
+  PositionComponent? connectedTo;
+
+  /// Stała sprężystości (im większa, tym mocniej ciągnie)
+  double stiffness = 200.0;
+
+  /// Współczynnik tłumienia (0 = brak, 1 = mocne tłumienie)
+  double damping = 5.0;
+
+  /// Długość spoczynkowa sprężyny
+  Vector2? restPosition;
+  double? restLength;
+
+  /// Aktualna prędkość (wektor)
+  Vector2 velocity = Vector2.zero();
+
+  void attach(PositionComponent other) {
+    connectedTo = other;
+    restPosition = other.position - position;
+    restLength = restPosition!.length;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    if (connectedTo == null) return;
+
+    final other = connectedTo!;
+    final displacement = other.position - position - restPosition!;
+    final distance = displacement.length;
+
+    if (distance == 0) return;
+
+    // Kierunek działania sprężyny
+    final direction = displacement.normalized();
+
+    // Siła Hooke'a: F = -k * (x - L)
+    final stretch = distance - restLength!;
+    final force = direction * (stiffness * stretch);
+
+    // Dodaj tłumienie (proporcjonalne do prędkości)
+    final dampingForce = velocity * -damping;
+
+    // Suma sił
+    final totalForce = force + dampingForce;
+
+    // Zakładamy jednostkową masę, więc a = F
+    final acceleration = totalForce;
+
+    // Integracja ruchu (Euler)
+    velocity += acceleration * dt;
+    position += velocity * dt;
+  }
 }
